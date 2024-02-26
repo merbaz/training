dbe007c18ff517f4fee3bc7dab27815cbb6bb671
merbaz <merbaz@nisum.com>
2024-02-26 13:19:58 +0500

    ASSIGNMENT 12 | TASK 2

--- src/assignment12/app.ts
+++ src/assignment12/app.ts
@@ -7,19 +7,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/test", (_, res)=>{
-    let total = 0;
-    for(let i = 0; i<5_000_000; i++){
-        total++;
-    }
-    res.send(`the total is: ${total}\n`)
-})
+app.get("/test", (_, res) => {
+  let total = 0;
+  for (let i = 0; i < 5_000_000; i++) {
+    total++;
+  }
+  res.send(`the total is: ${total}\n`);
+});
 
+app.get("/", (_, res) => {
+  res.send("Hello World");
+});
 
-app.listen(port, ()=>{
-    console.log(`port: ${port}`);
-    console.log(`pid: ${process.pid}`)
-})
-
-
-
+app.listen(port, () => {
+  console.log(`port: ${port}`);
+  console.log(`pid: ${process.pid}`);
+});
